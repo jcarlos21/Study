265697924bb7c904e47434fe821c1899baf7576b
jcarlos21 <j.carlos2020@live.com>
2022-03-13 23:30:58 -0300

    Comitando desafiosDIO em C#

---
+++ DesafiosDIO/DESAFIO3_DIO/DESAFIO3_DIO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace DESAFIO3_DIO {
+    internal class Program {
+        static void Main(string[] args) {           
+            
+            int testes = int.Parse(Console.ReadLine());            
+
+            for (int i = 0; i < testes; i++) {
+
+                string[] vet = Console.ReadLine().Split(' ');
+                string[] listaCompras = vet.Distinct().ToArray();
+
+                Array.Sort(listaCompras);
+                string lista = String.Join(" ", listaCompras);
+                Console.WriteLine(lista);
+
+            }
+        }
+    }
+}
---
+++ DesafiosDIO/Desafio2DIO/Desafio2DIO/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Desafio2DIO {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int testes = int.Parse(Console.ReadLine());
+            int cont = 0;
+
+            for (int i = 0; i < testes; i++) {
+
+                int clientes = int.Parse(Console.ReadLine());
+                string[] vet = Console.ReadLine().Split(' ');
+
+                int[] vetor = new int[vet.Length];
+                int[] vetor2 = new int[vet.Length];
+
+                for (int j = 0; j < vetor.Length; j++) {
+                    vetor[j] = int.Parse(vet[j]);
+                    vetor2[j] = int.Parse(vet[j]);
+                }
+
+                Array.Sort(vetor);
+                Array.Reverse(vetor);
+
+                for (int j = 0; j < vetor2.Length; j++) {
+
+                    if (vetor2[j] == vetor[j]) {
+                        cont++;
+                    }
+                }
+
+                Console.WriteLine(cont);
+                cont = 0;
+            }
+
+
+        }
+    }
+}
