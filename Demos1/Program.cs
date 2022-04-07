0cce192320e441343b5649ea2b0def4540cbdb49
jcarlos21 <j.carlos2020@live.com>
2022-04-07 20:22:40 -0300

    Demos do curso Referência e Valor em C#

--- Demos1/Program.cs
+++ Demos1/Program.cs
@@ -3,7 +3,45 @@
 namespace Demos1;
 
 internal class Program {
+
+    static void Demo1 () {
+        int a = 5;
+        Adicionar20(ref a);
+        Console.WriteLine($"O valor de a é {a}");
+    }
+
+    static void Adicionar20 (ref int a) {
+        a += 20;
+    }
+
+    static void AlterarNome (string[] nomes, string nome, string nomeNovo) {
+        // Algoritmo de busca linear
+        for (int i = 0; i < nomes.Length; i++) {
+            if (nomes[i] == nome) {
+                nomes[i] = nomeNovo;
+            }
+        }
+    }
+
     static void Main(string[] args) {
-        Console.WriteLine("Hello World!");
+        var nomes = new string[]{"José", "Maria", "Ricardo", "Alice", "Pedro"};
+        Console.WriteLine($@"A lista de nomes é:
+            {string.Join(", \n", nomes)}
+        ");
+
+
+        System.Console.WriteLine("Digite o nome a ser substituido: ");
+        var nome = Console.ReadLine();
+        System.Console.WriteLine("Digite o nome novo: ");
+        var nomeNovo = Console.ReadLine();
+
+        AlterarNome(nomes, nome, nomeNovo);
+
+        Console.WriteLine($@"A lista de nomes alterada é:
+            {string.Join(", \n", nomes)}
+        ");
+
+        System.Console.WriteLine();
+        
     }
 }
